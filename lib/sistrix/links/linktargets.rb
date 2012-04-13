973c1ec43e9212f372286dc900d6e25d41ff686b
Tobias Begalke <tb@tb.unnet>
2012-04-13 11:09:39 +0200

    link api calls implemented

--- lib/sistrix/links/linktargets.rb
+++ lib/sistrix/links/linktargets.rb
@@ -2,15 +2,16 @@ module Sistrix
 
   require 'sistrix/base'
   class Links
-    class List
+    class Linktargets
 
       include ::Sistrix::Base
 
-      attr_reader :credits, :links
+      attr_reader :credits, :targets
 
       def initialize(options = {})
         @options = {
           'domain' => nil,
+          'num' => nil,
           'api_key' => Sistrix.config.api_key,
         }.merge(options)
 
@@ -24,9 +25,9 @@ module Sistrix
 
         @credits = data.xpath('/response/credits').first['used'].to_i
 
-        @links = []
-        data.xpath('/response/answer/link').each do |r|
-          @links << Record.new(r)
+        @targets = []
+        data.xpath('/response/answer/target').each do |r|
+          @targets << Record.new(r)
         end
 
         self
@@ -43,9 +44,12 @@ module Sistrix
         def initialize(xml_node)
           @data = {}
 
-          @data[:url_from] = xml_node['url.from'].to_s.strip
-          @data[:url_to] = xml_node['url.to'].to_s.strip
-          @data[:text] = xml_node['text'].to_s.strip
+          @data[:url] = xml_node['url'].to_s.strip
+          @data[:links] = xml_node['links'].to_s.strip.to_i
+          @data[:hosts] = xml_node['hosts'].to_s.strip.to_i
+          @data[:domains] = xml_node['domains'].to_s.strip.to_i
+          @data[:nets] = xml_node['nets'].to_s.strip.to_i
+          @data[:ips] = xml_node['ips'].to_s.strip.to_i
         end
       end
 
--- spec/sistrix_links_spec.rb
+++ spec/sistrix_links_spec.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 require 'sistrix'
 require 'pp'
 
+
+
 WebMock.disable_net_connect!
 include WebMock::API
 
@@ -21,11 +23,199 @@ describe "Sistrix.links_overview(:domain => [domain]) returns an overview of the
 
   it { should be }
 
-  its(:credits) { should == 25 }
-  its(:total) { should == 2357268 }
-  its(:hosts) { should == 84489 }
-  its(:domains) { should == 51929 }
-  its(:networks) { should == 28613 }
-  its(:class_c) { should == 11878 }
+  its(:credits) { should be 25 }
+  its(:total) { should be 2357268 }
+  its(:hosts) { should be 84489 }
+  its(:domains) { should be 51929 }
+  its(:networks) { should be 28613 }
+  its(:class_c) { should be 11878 }
 end
 
+
+
+describe "Sistrix.links_list(:domain => [domain]) returns the complete list of backlinks for the given domain" do
+  subject {
+    stub_request(:get, Sistrix::SERVICE_HOST + "/links.list?api_key=&domain=#{domain}").to_return(File.new(File.join(xml_base, 'links.list.xml')))
+    Sistrix.links_list(:domain => domain)
+  }
+
+  it { should be }
+
+  its(:credits) { should be 250 }
+
+  its(:links) { should be }
+  it { should have(10000).links }
+
+  describe "links[0]" do
+    let(:link) { subject.links[0] }
+
+    it "url_from should be 'http://zeit-spiel.de/shop.html'" do
+      link.url_from { should be 'http://zeit-spiel.de/shop.html' }
+    end
+
+    it "text should be 'About Time im DIE ZEIT Online Shop'" do
+      link.text { should be 'About Time im DIE ZEIT Online Shop' }
+    end
+
+    it "url_to should be 'https://shop.zeit.de/category/745-DIE-ZEIT-Spiel-ABOUT-TIME'" do
+      link.url_to { should be 'https://shop.zeit.de/category/745-DIE-ZEIT-Spiel-ABOUT-TIME' }
+    end
+  end
+
+  describe "links[9999]" do
+    let(:link) { subject.links[9999] }
+
+    it "url_from should be 'http://mooszka.blogspot.com/2008/09/johnny-clarke-yard-style.html'" do
+      link.url_from { should be 'http://mooszka.blogspot.com/2008/09/johnny-clarke-yard-style.html' }
+    end
+
+    it "text should be 'Zuender (D)'" do
+      link.text { should be 'Zuender (D)' }
+    end
+
+    it "url_to should be 'http://zuender.zeit.de/'" do
+      link.url_to { should be 'http://zuender.zeit.de/' }
+    end
+  end
+
+end
+
+
+describe "Sistrix.links_linktargets(:domain => [domain], :num => [number_of_results]) returns a list of pages with backlink statistics within the given domain" do
+    subject {
+    stub_request(:get, Sistrix::SERVICE_HOST + "/links.linktargets?api_key=&domain=#{domain}&num=").to_return(File.new(File.join(xml_base, 'links.linktargets.xml')))
+    Sistrix.links_linktargets(:domain => domain)
+  }
+
+  it { should be }
+  its(:credits) { should be 100 }
+  its(:targets) { should be }
+  it { should have(100).targets }
+
+  describe "targets[0]" do
+    let(:target) { subject.targets[0] }
+
+    it "url should be 'http://zeit.de/'" do
+      target.url { should be 'http://zeit.de/' }
+    end
+
+    it "links should be 4148" do
+      target.links { should be 4148 }
+    end
+
+    it "hosts should be 321"  do
+      target.hosts { should be 321 }
+    end
+
+    it "domains should be 230" do
+      target.domains { should be 230 }
+    end
+
+    it "nets should be 185" do
+      target.nets { should be 185 }
+    end
+
+    it "ips should be 204" do
+      target.ips  { should be 204 }
+    end
+  end
+
+  describe "targets[99]" do
+    let(:target) { subject.targets[99] }
+
+    it "url should be 'http://www.zeit.de/wissen/2011-08/depressionen-pille'" do
+      target.url { should be 'http://www.zeit.de/wissen/2011-08/depressionen-pille' }
+    end
+
+    it "links should be 101" do
+      target.links { should be 101 }
+    end
+
+    it "hosts should be 33"  do
+      target.hosts { should be 33 }
+    end
+
+    it "domains should be 29" do
+      target.domains { should be 29 }
+    end
+
+    it "nets should be 24" do
+      target.nets { should be 24 }
+    end
+
+    it "ips should be 29" do
+      target.ips  { should be 29 }
+    end
+  end
+
+end
+
+
+describe "Sistrix.links_linktexts(:domain => [domain], :num => [number_of_results]) returns a list of backlink texts to the given domain" do
+    subject {
+    stub_request(:get, Sistrix::SERVICE_HOST + "/links.linktexts?api_key=&domain=#{domain}&num=10").to_return(File.new(File.join(xml_base, 'links.linktexts.xml')))
+    Sistrix.links_linktexts(:domain => domain, :num => 10)
+  }
+
+  it { should be }
+  its(:credits) { should be 10 }
+  its(:linktexts) { should be }
+  it { should have(10).linktexts }
+
+  describe "linktexts[0]" do
+    let(:linktext) { subject.linktexts[0] }
+
+    it "text should be ''" do
+      linktext.text { should be '' }
+    end
+
+    it "links should be 2992" do
+      linktext.links { should be 2992 }
+    end
+
+    it "hosts should be 836"  do
+      linktext.hosts { should be 836 }
+    end
+
+    it "domains should be 576" do
+      linktext.domains { should be 576 }
+    end
+
+    it "nets should be 327" do
+      linktext.nets { should be 327 }
+    end
+
+    it "ips should be 406" do
+      linktext.ips  { should be 406 }
+    end
+  end
+
+  describe "linktexts[9]" do
+    let(:linktext) { subject.linktexts[9] }
+
+    it "text should be 'N/A'" do
+      linktext.text { should be 'N/A' }
+    end
+
+    it "links should be 172" do
+      linktext.links { should be 172 }
+    end
+
+    it "hosts should be 140"  do
+      linktext.hosts { should be 140 }
+    end
+
+    it "domains should be 117" do
+      linktext.domains { should be 117 }
+    end
+
+    it "nets should be 68" do
+      linktext.nets { should be 68 }
+    end
+
+    it "ips should be 77" do
+      linktext.ips  { should be 77 }
+    end
+  end
+
+end
